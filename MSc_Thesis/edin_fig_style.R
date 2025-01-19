8746c0f584a11ecada8960368ece8d50b02768d8
Tim Old <tim.old@doctors.org.uk>
2025-01-19 10:40:01 +0000

    Refs Update 20250119

---
+++ MSc_Thesis/edin_fig_style.R
@@ -0,0 +1,32 @@
+# get University of Edinburgh colours
+source("../../edin_uni_colours.R")
+
+# plot template
+plot_template <- function(data)  {
+  data %>%
+    ggplot() +
+    ## set generic theme
+    scale_fill_edin_d("digital") +
+    scale_colour_edin_d("digital") +
+    scale_fill_edin_c("blue_ramp") +
+    scale_colour_edin_c("blue_ramp") +
+    theme_bw() +
+    theme(
+      plot.title = element_text(colour = edin_spruce_grey_hex,
+                                size = 14),
+      plot.subtitle = element_text(colour = edin_spruce_grey_hex,
+                                   size = 10),
+      plot.caption = element_text(face = "italic",
+                                  colour = edin_spruce_grey_hex,
+                                  size = 10),
+      axis.title = element_text(colour = edin_spruce_grey_hex,
+                                size = 12),
+      axis.text = element_text(colour = edin_spruce_grey_hex,
+                               size = 10),
+      panel.grid = element_line(colour = edin_light_blue_hex),
+      strip.text = element_text(colour = edin_spruce_grey_hex,
+                                size = 10),
+      strip.background = element_rect(fill = edin_light_blue_hex,
+                                      colour = edin_spruce_grey_hex)
+    )
+}
